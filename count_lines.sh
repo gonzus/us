4d5c48deb1006b91aea1b8fce51a4e4cb99f7391
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2016-04-19 14:52:59 +0200

    Script to count non-empty LOC

---
+++ count_lines.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+find . -type f |
+egrep -v '/(\.git)/' |
+egrep '\.(h|c)$' |
+egrep -v '(gonzo)\.(c)$' |
+while read f
+do
+    echo -n $f
+    cat $f |
+    sed -e 's+//.*++g' |
+    egrep -v '^[ \t]*$' |
+    wc -l
+done |
+awk '
+{
+    t += $2;
+    printf("%8d %s\n", $2, $1);
+}
+END {
+    printf("%8d %s\n", t, "TOTAL");
+}'
+
