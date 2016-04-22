66b01637a399c931b164a8c082ce6fd669194f29
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2016-04-22 23:54:54 +0200

    Refactor apply for proc / native

--- eval.c
+++ eval.c
@@ -9,6 +9,8 @@
 #define EVAL_LAMBDA "lambda"
 
 static Cell* cell_apply(Cell* cell, Env* env);
+static Cell* cell_apply_proc(Cell* cell, Env* env, Cell* proc);
+static Cell* cell_apply_native(Cell* cell, Env* env, Cell* proc);
 static Cell* cell_set_value(Cell* cell, Env* env, int create);
 static Cell* cell_if(Cell* cell, Env* env);
 static Cell* cell_lambda(Cell* cell, Env* env);
@@ -77,19 +79,60 @@ Cell* cell_eval(Cell* cell, Env* env)
 // Apply a function (car) to all its arguments (cdr)
 static Cell* cell_apply(Cell* cell, Env* env)
 {
-    Cell* ret = nil;
     Cell* proc = cell_eval(cell->cons.car, env);
     if (!proc) {
-        return ret;
+        return nil;
     }
+    switch (proc->tag) {
+        case CELL_PROC:
+            return cell_apply_proc(cell, env, proc);
+
+        case CELL_NATIVE:
+            return cell_apply_native(cell, env, proc);
+    }
+    return nil;
+}
+
+static Cell* cell_apply_proc(Cell* cell, Env* env, Cell* proc)
+{
+    Cell* ret = nil;
+    Env* proc_env = env_create(0, proc->pval.env);
+    Cell* p;
+    Cell* a;
+    int pos = 0;
+    for (p = proc->pval.params, a = cell->cons.cdr;
+         p && p != nil && a && a != nil;
+         p = p->cons.cdr, a = a->cons.cdr, ++pos) {
+        Cell* par = p->cons.car;
+        Cell* arg = cell_eval(a->cons.car, env);
+        if (!arg) {
+            printf("Could not evaluate arg #%d [%s]\n", pos, par->sval);
+            return nil;
+        }
+        Symbol* sym = env_lookup(proc_env, par->sval, 1);
+        if (!sym) {
+            printf("Could not create binding for arg #%d [%s]\n", pos, par->sval);
+            return nil;
+        }
+        sym->value = arg;
+        // printf("Proc, arg #%d [%s]\n", pos, par->sval);
+    }
+    ret = cell_eval(proc->pval.body, proc_env);
+    env_destroy(proc_env);
+    return ret;
+}
+
+static Cell* cell_apply_native(Cell* cell, Env* env, Cell* proc)
+{
+    Cell* ret = nil;
     Cell* args = 0;
     Cell* last = 0;
-    for (Cell* c = cell->cons.cdr; c && c != nil; c = c->cons.cdr) {
+    int pos = 0;
+    for (Cell* c = cell->cons.cdr; c && c != nil; c = c->cons.cdr, ++pos) {
         Cell* arg = cell_eval(c->cons.car, env);
         if (!arg) {
-            // TODO: error
-            printf("You stupid motherfucker\n");
-            continue;
+            printf("Could not evaluate arg #%d\n", pos);
+            return ret;
         }
         Cell* cons = cell_cons(arg, nil);
         if (!args) {
@@ -99,40 +142,9 @@ static Cell* cell_apply(Cell* cell, Env* env)
             last->cons.cdr = cons;
         }
         last = cons;
+        // printf("Native, arg #%d\n", pos);
     }
-
-    Env* proc_env;
-    Cell* p;
-    Cell* a;
-    switch (proc->tag) {
-        case CELL_PROC:
-            proc_env = env_create(0, proc->pval.env);
-            for (p = proc->pval.params, a = args;
-                 p && p != nil && a && a != nil;
-                 p = p->cons.cdr, a = a->cons.cdr) {
-                Cell* par = p->cons.car;
-                Cell* arg = a->cons.car;
-                Symbol* symbol = env_lookup(proc_env, par->sval, 1);
-                if (!symbol) {
-                    printf("Could not create symbol for [%s]\n", par->sval);
-                    continue;
-                }
-                printf("Proc, arg %s\n", par->sval);
-                symbol->value = arg;
-            }
-            ret = cell_eval(proc->pval.body, proc_env);
-            env_destroy(proc_env);
-            break;
-
-        case CELL_NATIVE:
-            ret = proc->nval.func(args);
-            break;
-
-        default:
-            // TODO: error
-            printf("You stupid motherfucker\n");
-            break;
-    }
+    ret = proc->nval.func(args);
     return ret;
 }
 
